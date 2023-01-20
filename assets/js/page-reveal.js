9e65a30321a0bb13e8f95014ecdde99bec86e5bd
Jeremy <aworetanjeremiah@gmail.com>
2023-01-20 11:38:45 +0100

    feat: use DOMContentLoaded for timeline animation

--- assets/js/page-reveal.js
+++ assets/js/page-reveal.js
@@ -1,1 +1,8 @@
-let tl=gsap.timeline();tl.fromTo(".pre-load",{opacity:1},{opacity:0,duration:0.5,delay:3,display:"none",ease:"power4.out"});
+document.addEventListener('DOMContentLoaded', function () {
+  let tl = gsap.timeline();
+  tl.fromTo(
+    '.pre-load',
+    { opacity: 1 },
+    { opacity: 0, duration: 0.5, delay: 3, display: 'none', ease: 'power4.out' }
+  );
+});
